966ae3a2e90ad6be613ae66b1bacae5fffab8125
hanumonke <hanumansanchez@gmail.com>
2025-03-27 20:51:24 -0400

    feat: basic structure of the component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Institutional/Hero/Hero'
 import Services from './components/Institutional/Services/Services'
 import Footer from './components/Institutional/Footer/Footer'
 import Header from './components/Institutional/Header/Header'
+import About from './components/Institutional/About/About'
 
 function App() {
   
@@ -12,10 +13,11 @@ function App() {
 
       <Header/>
 
-  
+
     <main>
     <Hero />
     <Services />
+    <About />
 
     </main>
     {/* Footer position is bottom */}
---
+++ src/components/Institutional/About/About.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import styles from './About.module.css'
+import { useState } from 'react'
+
+import objetivoImage from '../../../assets/img/imagenEstudiantes.png'
+import misionImage from '../../../assets/img/imgenGraduando.png'
+import visionImage from '../../../assets/img/imagenCuenta.png'
+
+
+const About = () => {
+
+    const mision = {
+        title: 'Mision', 
+        content: '...', 
+        backgroundUrl: misionImage
+    }
+
+    const vision = {
+        title: 'Vision', 
+        content: '...', 
+        backgroundUrl: visionImage
+    }
+
+    const objetivos = {
+        title: 'Objetivos', 
+        content: '...', 
+        backgroundUrl: objetivoImage
+    }
+
+    const [currentItem, setCurrentItem] = useState(vision);
+
+    const changeItem = (item) => {
+        switch (item) {
+            case 'Mision': setCurrentItem(mision); break; 
+            case 'Vision': setCurrentItem(vision); break; 
+            case 'Objetivos': setCurrentItem(objetivos); break; 
+        }
+    }
+
+  return (
+    <div className={styles.box1}> 
+    {/* Primer contenedor */}
+       
+        <h1>Sobre Nosotros</h1>
+        {/* Segundo contenedor */}
+        <div className={styles.box2}> 
+            {/* tercer contenedor */}
+            <nav className={styles.navbar}>
+                <h2 onClick={() => { changeItem('Mision')  }}>Mision</h2>
+                <h2 onClick={() => { changeItem('Vision')  }}>Vision</h2>
+                <h2 onClick={() => { changeItem('Objetivos')  }}>Objetivos</h2>
+            </nav>
+            {/* Cuarto contenedor */}
+            <div className={styles.box4} style={{backgroundImage: 'url(' + currentItem.backgroundUrl + ')' }}>
+                <h2>{currentItem.title}</h2>
+                {/* <img src={currentItem.backgroundUrl} alt="imagen" /> */}
+            </div>
+
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default About
