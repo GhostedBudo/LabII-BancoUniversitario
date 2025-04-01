27950047ecbdf4374fb0207a2bd5f00cbce84c10
hanumonke <hanumansanchez@gmail.com>
2025-04-01 16:28:15 -0400

    responsive almost done, alternation of the value cards done via css z-index

--- src/components/Institutional/About/About.jsx
+++ src/components/Institutional/About/About.jsx
@@ -1,65 +1,80 @@
-import styles from './About.module.css'
-import { useState } from 'react'
+import styles from "./About.module.css";
+import { useState } from "react";
 
-import objetivoImage from '../../../assets/img/imagenEstudiantes.png'
-import misionImage from '../../../assets/img/imgenGraduando.png'
-import visionImage from '../../../assets/img/imagenCuenta.png'
+import objetivoImage from "../../../assets/img/imagenEstudiantes.png";
+import misionImage from "../../../assets/img/imgenGraduando.png";
+import visionImage from "../../../assets/img/imagenCuenta.png";
 
-import ValueCard from './ValueCard'
+import ValueCard from "./ValueCard";
 
 const About = () => {
+  const [currentItem, setCurrentItem] = useState("Mision");
+  const items = [
+    {
+      title: "Mision",
+      content:
+        "Somos una confiable institución financiera comprometida con los estudiantes universitarios, brindando soluciones financieras ágiles y eficientes. Nuestra misión es facilitar la gestión de sus recursos y contribuir al crecimiento económico y personal de nuestros clientes.",
+      backgroundUrl: misionImage,
+    },
 
-    const mision = {
-        title: 'Mision', 
-        content: 'Somos una confiable institución financiera comprometida con los estudiantes universitarios, brindando soluciones financieras ágiles y eficientes. Nuestra misión es facilitar la gestión de sus recursos y contribuir al crecimiento económico y personal de nuestros clientes.', 
-        backgroundUrl: misionImage
-    }
+    {
+      title: "Vision",
+      content:
+        "Queremos ser la principal opción financiera para estudiantes universitarios en el país. Deseamos ser reconocidos por nuestros servicios innovadores, la calidad de atención al cliente y nuestro compromiso con la educación y el desarrollo social.",
+      backgroundUrl: visionImage,
+    },
 
-    const vision = {
-        title: 'Vision', 
-        content: 'Queremos ser la principal opción financiera para estudiantes universitarios en el país. Deseamos ser reconocidos por nuestros servicios innovadores, la calidad de atención al cliente y nuestro compromiso con la educación y el desarrollo social.', 
-        backgroundUrl: visionImage
-    }
+    {
+      title: "Objetivos",
+      content:
+        "Brindar a los universitarios un servicio eficiente y de calidad en la gestión de sus recursos financieros, a través de una banca digital. Mantener una cultura de innovación y mejora continua en nuestros procesos, productos y servicios.",
+      backgroundUrl: objetivoImage,
+    },
+  ];
 
-    const objetivos = {
-        title: 'Objetivos', 
-        content: 'Brindar a los universitarios un servicio eficiente y de calidad en la gestión de sus recursos financieros, a través de una banca digital. Mantener una cultura de innovación y mejora continua en nuestros procesos, productos y servicios.', 
-        backgroundUrl: objetivoImage
-    }
-
-    const [currentItem, setCurrentItem] = useState(mision);
-
-    const changeItem = (item) => {
-        switch (item) {
-            case 'Mision': setCurrentItem(mision); break; 
-            case 'Vision': setCurrentItem(vision); break; 
-            case 'Objetivos': setCurrentItem(objetivos); break; 
-        }
-    }
+  const changeItem = (item) => {
+    setCurrentItem(item);
+  };
+  const itemsList = items.map((i) => (
+    <ValueCard isActive={currentItem === i.title} key={i.title} info={i} />
+  ));
 
   return (
-    <div className={styles.box1}> 
-    {/* Primer contenedor */}
-       
-        <h1>Sobre Nosotros</h1>
-        {/* Segundo contenedor */}
-        <div className={styles.box2}> 
-            {/* Tercer contenedor */}
-            <nav className={styles.navbar}>
-                <h2 onClick={() => { changeItem('Mision')  }}>Mision</h2>
-                <h2 onClick={() => { changeItem('Vision')  }}>Vision</h2>
-                <h2 onClick={() => { changeItem('Objetivos')  }}>Objetivos</h2>
-            </nav>
+    <div className={styles.box1}>
+      {/* Primer contenedor */}
 
-            
-            <ValueCard 
-            key={currentItem.title}
-            info={currentItem}/>
+      <h1>Sobre Nosotros</h1>
+      {/* Segundo contenedor */}
+      <div className={styles.box2}>
+        {/* Tercer contenedor */}
+        <nav className={styles.navbar}>
+          <h2
+            onClick={() => {
+              changeItem("Mision");
+            }}
+          >
+            Mision
+          </h2>
+          <h2
+            onClick={() => {
+              changeItem("Vision");
+            }}
+          >
+            Vision
+          </h2>
+          <h2
+            onClick={() => {
+              changeItem("Objetivos");
+            }}
+          >
+            Objetivos
+          </h2>
+        </nav>
 
-            
-        </div>
+        <div className={styles.cards}>{itemsList}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
